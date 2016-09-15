1e0bf4949465334cdac0cb25baa24424a762bb4a
Luiz Guerra <lhguerra@gmail.com>
2016-09-15 14:32:50 -0300

    Added new release based on master of development repository.

--- profiles/pece/themes/pece_scholarly_lite/assets/lib/remodal/dist/remodal.js
+++ profiles/pece/themes/pece_scholarly_lite/assets/lib/remodal/dist/remodal.js
@@ -1,5 +1,5 @@
 /*
- *  Remodal - v1.1.0
+ *  Remodal - v1.0.7
  *  Responsive, lightweight, fast, synchronized with CSS animations, fully customizable modal window plugin with declarative configuration and hash tracking.
  *  http://vodkabears.github.io/remodal/
  *
@@ -79,8 +79,7 @@
     closeOnCancel: true,
     closeOnEscape: true,
     closeOnOutsideClick: true,
-    modifier: '',
-    appendTo: null
+    modifier: ''
   }, global.REMODAL_GLOBALS && global.REMODAL_GLOBALS.DEFAULTS);
 
   /**
@@ -507,7 +506,6 @@
    */
   function Remodal($modal, options) {
     var $body = $(document.body);
-    var $appendTo = $body;
     var remodal = this;
 
     remodal.settings = $.extend({}, DEFAULTS, options);
@@ -516,13 +514,9 @@
 
     remodal.$overlay = $('.' + namespacify('overlay'));
 
-    if (remodal.settings.appendTo !== null && remodal.settings.appendTo.length) {
-      $appendTo = $(remodal.settings.appendTo);
-    }
-
     if (!remodal.$overlay.length) {
       remodal.$overlay = $('<div>').addClass(namespacify('overlay') + ' ' + namespacify('is', STATES.CLOSED)).hide();
-      $appendTo.append(remodal.$overlay);
+      $body.append(remodal.$overlay);
     }
 
     remodal.$bg = $('.' + namespacify('bg')).addClass(namespacify('is', STATES.CLOSED));
@@ -542,7 +536,7 @@
         namespacify('is', STATES.CLOSED))
       .hide()
       .append(remodal.$modal);
-    $appendTo.append(remodal.$wrapper);
+    $body.append(remodal.$wrapper);
 
     // Add the event listener for the close button
     remodal.$wrapper.on('click.' + NAMESPACE, '[data-' + PLUGIN_NAME + '-action="close"]', function(e) {
